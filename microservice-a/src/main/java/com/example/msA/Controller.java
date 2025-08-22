74a870053bffefd439b08b219ce688f28b6d2f83
Prashanth <prashanthacharya96@gmail.com>
2025-08-22 23:50:33 +0530

    Initial Commit

--- microservice-a/src/main/java/com/example/msA/Controller.java
+++ microservice-a/src/main/java/com/example/msA/Controller.java
@@ -10,7 +10,7 @@ public class Controller {
     @GetMapping("/call-b")
     public String callB() {
         RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject("http://microservice-b:8082/hello", String.class);
+        String response = restTemplate.getForObject("http://microservice-b:8080/hello", String.class);
         ////        String response = restTemplate.getForObject("http://localhost:8080/hello", String.class);
         return "Response from B: " + response;
     }
